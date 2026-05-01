0cb337ff4f66b9918b4cbced162d3e53273c3470
gnanaprasanna-29 <gnanaprasannaduvvu@gmail.com>
2026-05-01 20:09:59 +0530

    Time: 2 ms (99.3%), Space: 47.3 MB (11.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,8 +5,7 @@ class Solution {
         {            
             int complement = target - nums[i];
             if(map.containsKey (complement))
-            {
-                
+            {           
                 return new  int[] {map.get(complement), i};
             } 
             else
